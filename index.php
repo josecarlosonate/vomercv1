8df6ce0157b481d3c0dc1c744280dcb2c9d111fc
Jose rodriguez <jonate@nexura.com>
2021-06-27 18:58:20 -0500

    feat:primer commit

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <meta name="title" content="VOMERC">
+
+    <meta name="description" content="Vomerc, es un CENTRO COMERCIAL DIGITAL o marketplace donde, GRATUITAMENTE, podrán interactuar los negocios, emprendedores y personas, en una dinámica de compra y venta de productos y servicios.">
+
+    <meta name="keyword" content="vomerc,comercio">
+
+    <title>VOMERC</title>
+
+    <!--=====================================
+    {{-- fontawesome --}}
+    ======================================-->
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+    <!--=====================================
+	HOJA DE CSS 
+	======================================-->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+
+    <!--=====================================
+	Fonts 
+	======================================-->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;600&family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arimo&display=swap" rel="stylesheet">
+
+
+</head>
+
+<body>
+    <div class="barra fixed-top">
+        <div class="container" id="">
+            <nav class="d-flex flex-wrap justify-content-between pt-2 bg-white ">
+                <a href="/" title="Vomerc" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none logo">
+                    <img class="img-fluid " src="img/logo.png" alt="">
+                </a>
+
+                <button id="btnNav" class="navbar-toggler text-danger" type="button" data-toggle="collapse" data-target="#collapseNavbar" aria-expanded="false" aria-controls="navbarText" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+
+                <div class="navbar-collapse collapse" id="collapseNavbar">
+                    <div class="card card-body">
+                        <ul class="navbar-nav text-center" id="nav">
+                            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Inicio</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">Características</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">Capturas de pantalla</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">Descargar</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">Opiniones</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <ul class="nav nav-pills mb-2" id="collapseNa">
+                    <li class="nav-item"><a href="#home" class="nav-link active" aria-current="page">Inicio</a></li>
+                    <li class="nav-item"><a href="#caracteristicas" class="nav-link">Características</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Capturas de pantalla</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Descargar</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Opiniones</a></li>
+                </ul>
+
+            </nav>
+        </div>
+    </div>
+
+    <!-- seccion 1 -->
+    <section class="sectcion1">
+        <div class="container">
+            <div class="hero-content">
+                <h1>Vomerc comercio digital <br>Totalmente Gratuito</h1>
+                <p>Cualquier persona tendrá acceso a una gran <br> cantidad de productos y servicios.</p>
+                <div class="btn-group">
+                    <a href="#"><img src="img/appstore.png" alt="appstore"></a>
+                    <a target="_blank" href="https://play.google.com/store/apps/details?id=com.vomerc.vomerc&hl=es_CO&gl=US"><img src="img/playstore.png" alt="playsotre"></a>
+                </div>
+            </div>
+        </div>
+        <div class="hero-moc">
+        </div>
+    </section> <!-- end seccion 1 -->
+
+    <!-- seccion 2 -->
+    <section class="promo-section padding" id="">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-6 sm-padding">
+                    <div class="promo-content text-center">
+                        <i class="fas fa-lock"></i>
+                        <h3>Altamente asegurado</h3>
+                        <p>Lorem ipsum dolor sit amet adipisicing of more of sedis do eiusmod tempor incididunt. Appipsum incididunt.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 sm-padding">
+                    <div class="promo-content text-center">
+                        <i class="fas fa-layer-group"></i>
+                        <h3>Fácilmente manejable</h3>
+                        <p>Lorem ipsum dolor sit amet adipisicing of more of sedis do eiusmod tempor incididunt. Appipsum incididunt.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 sm-padding">
+                    <div class="promo-content text-center">
+                        <i class="fas fa-align-justify"></i>
+                        <h3>Bien documentado</h3>
+                        <p>Lorem ipsum dolor sit amet adipisicing of more of sedis do eiusmod tempor incididunt. Appipsum incididunt.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- seccion 3 -->
+    <section class="content-section transparencia padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="content-mockups">
+                        <div class="mockup-back"></div>
+                        <div class="mockup-front"></div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="section-heading mb-40">
+                        <h2>¿Cómo funciona esta aplicación?</h2>
+                        <p>Is amazing, modern and clean landing page for showcase <br> your app anything else.</p>
+                    </div>
+                    <div class="content-lists">
+                        <div class="list-item">
+                            <i class="icon-tools"></i>
+                            <div class="list-content">
+                                <h3>Create Your Account</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing <br> and typesetting industry.</p>
+                            </div>
+                        </div>
+                        <div class="list-item">
+                            <i class="icon-pictures"></i>
+                            <div class="list-content">
+                                <h3>Upload Your Picture</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing <br> and typesetting industry.</p>
+                            </div>
+                        </div>
+                        <div class="list-item">
+                            <i class="icon-genius"></i>
+                            <div class="list-content">
+                                <h3>Get Your Own Time</h3>
+                                <p>Lorem Ipsum is simply dummy text of the printing <br> and typesetting industry.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
